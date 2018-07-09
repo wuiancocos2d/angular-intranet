93fe96abd18af13b9e7e754b63448d81fef71611
wuiancocos2d <wuiancocos2d@gmail.com>
2018-07-09 20:18:03 +0800

    service

--- src/app/core/services/ad-news.service.ts
+++ src/app/core/services/ad-news.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AdNewsModel} from '../models/adNews.model';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class AdNewsService {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd'; // Ant design  framework
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ContainerComponent} from './layout/container/container.component';
-import { AdCarouselComponent } from './ad-carousel/ad-carousel.component';
+import {AdCarouselComponent} from './ad-carousel/ad-carousel.component';
 /** 配置 angular i18n **/
 import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
@@ -14,27 +14,29 @@ import zh from '@angular/common/locales/zh';
 registerLocaleData(zh);
 
 /** mock 数据**/
-import { HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
-import { InMemoryDataAdNewsService } from '../core/mock/in-memory-data-adNews.service';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataAdNewsService} from '../core/mock/in-memory-data-adNews.service';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        ReactiveFormsModule,
-        NgZorroAntdModule, // Ant design framework
-        /**Fake Data**/
-        HttpClientInMemoryWebApiModule,
-        InMemoryDataAdNewsService
-    ],
-    declarations: [ HeaderComponent, ContainerComponent, AdCarouselComponent],
-    exports: [
-        HeaderComponent, NgZorroAntdModule, BrowserAnimationsModule, ContainerComponent, AdCarouselComponent
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NgZorroAntdModule, // Ant design framework
+    /**Fake Data**/
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataAdNewsService, {dataEncapsulation: false}
+    )
 
-    /** 配置 ng-zorro-antd 国际化 **/
-    providers: [{provide: NZ_I18N, useValue: zh_CN}]
+  ],
+  declarations: [HeaderComponent, ContainerComponent, AdCarouselComponent],
+  exports: [
+    HeaderComponent, NgZorroAntdModule, BrowserAnimationsModule, ContainerComponent, AdCarouselComponent
+  ],
+
+  /** 配置 ng-zorro-antd 国际化 **/
+  providers: [{provide: NZ_I18N, useValue: zh_CN}]
 })
 export class SharedModule {
 }
